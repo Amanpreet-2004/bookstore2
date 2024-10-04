8db58f9ba8eb81f4c44548abe2953cd7ac38f69a
Amanpreet-2004 <kaurcsamanpreet@gmail.com>
2024-10-04 13:04:26 +0530

    bookstore

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import bookRoute from "./route/book.route.js";
-import userRoute from "./route/user.route.js";
+// import bookRoute from "./route/book.route.js";
+//  import userRoute from "./route/user.route.js";
 
 const app = express();
 
@@ -27,8 +27,8 @@ try{
     console.log("Error: ", error);
 }
 
-app.use("/book", bookRoute);
-app.use("/user", userRoute);
+// app.use("/book", bookRoute);
+// app.use("/user", userRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
